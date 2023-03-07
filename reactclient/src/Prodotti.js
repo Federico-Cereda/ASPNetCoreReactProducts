8d42951a40ba56377ed7138e5db224ee7fd1c50b
AVANADE-CORP\federico.cereda <federico.cereda@avanade.com>
2023-03-07 18:45:02 +0100

    Creazione tabella prodotti.

--- reactclient/src/Prodotti.js
+++ reactclient/src/Prodotti.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 
-const Prodotti = () => {
+export default function Prodotti(props) {
 
     const [prodotti, setProdotti] = useState([])
     useEffect(() => {
@@ -11,19 +11,39 @@ const Prodotti = () => {
             .then(result => {
                 setProdotti(result);
             })
-    })
-        .catch(error => console.log('error', error));
+    }, []);
 
     return (
-        <main>
-            {
-                prodotti.map((prodotto) => (
-                    <p>{prodotto}</p>
-                ))
-            }
-        </main>
+        <div className="table-responsive mt-5">
+            <table className="table table-bordered border-dark">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Prezzo</th>
+                        <th scope="col">Peso</th>
+                        <th scope="col">Marca</th>
+                        <th scope="col">CRUD Operations</th>
+                    </tr>
+                </thead>
+                <tbody className="align-middle">
+                    {prodotti.map((prodotto) => (
+                        <tr key={prodotto.id}>
+                            <th scope="row">{prodotto.id}</th>
+                            <td>{prodotto.nome}</td>
+                            <td>{prodotto.prezzo}</td>
+                            <td>{prodotto.peso}</td>
+                            <td>{prodotto.idMarca}</td>
+                            <td>
+                                <button className="btn btn-primary btn-lg">Detagli</button>
+                                <button className="btn btn-secondary btn-lg mx-3 my-3">Aggiorna</button>
+                                <button className="btn btn-dark btn-lg">Elimina</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     )
 
 }
-
-export default Prodotti
