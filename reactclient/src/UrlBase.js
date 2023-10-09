e82a19661a13e371addfbfd9114382adfe3ce82b
Federico-Cereda <94916608+Federico-Cereda@users.noreply.github.com>
2023-10-09 18:50:46 +0200

    Url Fix.

--- reactclient/src/UrlBase.js
+++ reactclient/src/UrlBase.js
@@ -5,19 +5,19 @@ const ENDPOINTS = {
     PRODOTTO: 'Prodotto',
     MARCA: 'Marca',
     PROMOZIONE: 'Promozione'
-};
+}
 
 const development = {
     API_PRODOTTO: `${API_BASE_URL_DEVELOPMENT}/${ENDPOINTS.PRODOTTO}`,
     API_MARCA: `${API_BASE_URL_DEVELOPMENT}/${ENDPOINTS.MARCA}`,
     API_PROMOZIONE: `${API_BASE_URL_DEVELOPMENT}/${ENDPOINTS.PROMOZIONE}`
-};
+}
 
 const production = {
     API_PRODOTTO: `${API_BASE_URL_PRODUCTION}/${ENDPOINTS.PRODOTTO}`,
     API_MARCA: `${API_BASE_URL_PRODUCTION}/${ENDPOINTS.MARCA}`,
     API_PROMOZIONE: `${API_BASE_URL_PRODUCTION}/${ENDPOINTS.PROMOZIONE}`
-};
+}
 
 const UrlBase = process.env.NODE_ENV === "development" ? development : production;
 
